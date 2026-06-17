01393fc1d5f8b7d8e4d1aa33b89235eddd83d617
Svetlana Sultanyan <svetlannasultanyan@gmail.com>
2026-06-17 11:42:30 +0200

    Calculation

--- etl/transform.py
+++ etl/transform.py
@@ -1,1 +1,14 @@
+import pandas as pd
 
+def calculer_indicateurs(df_capteur, df_events):
+
+    stats = {
+        "spo2_min": df_capteur['spo2'].min(),
+        "spo2_moy": df_capteur['spo2'].mean(),
+        "spo2_mediane": df_capteur['spo2'].median(),
+
+    }
+
+
+    
+    return stats
