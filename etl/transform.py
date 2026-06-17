5efed0cd8e75c4911c3f30801553f5ff8ec3f45c
Nassim <achnassim34@gmail.com>
2026-06-17 14:54:22 +0200

    <Feat> Extraction depuis sql fonctionnel et indicateurs calculés

--- etl/transform.py
+++ etl/transform.py
@@ -6,7 +6,6 @@ def calculer_indicateurs(df_capteur, df_events):
         "spo2_min": df_capteur['spo2'].min(),
         "spo2_moy": df_capteur['spo2'].mean(),
         "spo2_mediane": df_capteur['spo2'].median(),
-
     }
 
     #print(df_events[''])
--- index.py
+++ index.py
@@ -7,7 +7,7 @@ def run_pipeline(id_nuit):
     queries_response : dict = recuperer_donnees(id_nuit)
     df_capteur = queries_response['df_capteur']
     df_event = queries_response['df_events']
-    print(f"Extraction terminée : {len(df_capteur)} lignes lues dans le CSV.")
+    print(f"Étape 1 : Extraction : \n    {len(df_capteur)} lignes lues dans le CSV.\n    {queries_response['nbr_events'][0]} lignes lues dans la base MySQL.")
 
     # 2. Transformation
     print("Étape 2 : Calcul des indicateurs...")
