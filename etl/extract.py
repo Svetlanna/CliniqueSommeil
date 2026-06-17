ecba68b8c7780601955f975703a83675b5767c2d
Nassim <achnassim34@gmail.com>
2026-06-17 14:35:12 +0200

    <Feat> Call fonctionnel à la db pour chopper les calculs

--- etl/extract.py
+++ etl/extract.py
@@ -18,10 +18,34 @@ def recuperer_donnees(id_nuit : int):
         password=os.environ.get("password"),
         database=os.environ.get("database")
     )
-    cur = conn.cursor()
+
+    cur = conn.cursor(buffered=True)
     query = f'SELECT * FROM evenement_respiratoire WHERE id_nuit = {id_nuit}'
     cur.execute(query)
     df_events = cur.fetchall()
+
+    # Des bugs apparaissent lorsque chaque query est execute séparement
+    # recours a une fnction local pour fix
+    def call_proc(cursor, proc):
+        cursor.execute(f"CALL {proc};")
+        result = cursor.fetchone()
+        while cursor.nextset() is not None:
+            pass
+        return result
+
+    nbapnees = call_proc(cur, "clinique.sp_compteur_apnees()")
+    nbhypopnees = call_proc(cur, "clinique.sp_compteur_hypopnae()")
+    nbrera = call_proc(cur, "clinique.sp_compteur_rera()")
+    nbr_events = call_proc(cur, "clinique.sp_compteur_all()")
+
+    queries_response = {
+        'df_capteur': df_capteur,
+        'df_events': df_events,
+        'nbapnees': nbapnees,
+        'nbhypopnees': nbhypopnees,
+        'nbrera': nbrera,
+        'nbr_events': nbr_events,
+    }
     conn.close()
 
-    return df_capteur, df_events
+    return queries_response
