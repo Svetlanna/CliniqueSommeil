da53da74e8c6dea66f8a3a72b26853fc730d5641
Svetlana Sultanyan <svetlannasultanyan@gmail.com>
2026-06-22 12:34:52 +0200

    All Medecines list et medecin par id Api's

--- Api/index.js
+++ Api/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import 'dotenv/config';
 import nuitRoutes from './routes/nuitRoutes.js';
+import MedecinsRoute from './routes/MedecinsRoute.js';
+import appareilRoutes from './routes/appareilRoutes.js';
+import {medecineRoute} from "./controllers/medecineController.js";
 
 const app = express();
 app.use(express.json());
-
 app.use('/api/nuit', nuitRoutes);
+app.use('/api/med', medecineRoute);
+app.use('/api/appareil', appareilRoutes);
 
 app.listen(8888, () => console.log(`Server running on http://localhost:8888`));
